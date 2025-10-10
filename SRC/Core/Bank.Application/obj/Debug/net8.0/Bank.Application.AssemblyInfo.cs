621c2a0e573c42dc237af40babf7826979bd0efb
TejasPatil111 <tejas.patil@neosoftmail.com>
2025-10-10 19:18:10 +0530

    Oct-10

--- SRC/Core/Bank.Application/obj/Debug/net8.0/Bank.Application.AssemblyInfo.cs
+++ SRC/Core/Bank.Application/obj/Debug/net8.0/Bank.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bank.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eae4560f4d8f10969f45e34759ac9f9af58c7cea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc16920e43b2a1ec94d216d935222141b959321d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bank.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bank.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
