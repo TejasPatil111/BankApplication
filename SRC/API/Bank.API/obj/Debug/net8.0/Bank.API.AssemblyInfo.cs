2f30d6d5fc57f3694f65f246222dbaa6acddce9e
TejasPatil111 <tejas.patil@neosoftmail.com>
2025-10-15 11:19:40 +0530

    Oct-15

--- SRC/API/Bank.API/obj/Debug/net8.0/Bank.API.AssemblyInfo.cs
+++ SRC/API/Bank.API/obj/Debug/net8.0/Bank.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bank.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+621c2a0e573c42dc237af40babf7826979bd0efb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17202acf8869d3e2f06b5057107103e6d2d317e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bank.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bank.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
